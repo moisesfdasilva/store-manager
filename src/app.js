01c87853389c21ceb777c0de1042ec5855aa01cd
moisesfdasilva <msesfernandes@gmail.com>
2023-01-16 18:45:56 -0300

    feat: criado o middware nameAuthMiddware para validar o nome do produto.

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ const express = require('express');
 const app = express();
 
 const productsController = require('./controllers/productsController');
+const nameAuthMiddware = require('./middlewares/nameAuthMiddware');
 
 app.use(express.json());
 
 app.get('/products', productsController.getAll);
 app.get('/products/:id', productsController.getById);
-app.post('/products', productsController.insertProduct);
+app.post('/products', nameAuthMiddware, productsController.insertProduct);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
